f93aa333506edf1063812178801d2c5ff8e4940a
lex@U+2699 <lekakis@gmail.com>
2022-02-13 15:34:31 -0800

    ch1 tutorial cmt.1

---
+++ ch1/echo1.go
@@ -0,0 +1,16 @@
+// Echo1 prints the command line argument of the program
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
