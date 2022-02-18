2700bffbde2cbee8e89d2506d5182ec67c1bfb68
lex@U+2699 <lekakis@gmail.com>
2022-02-17 21:58:59 -0800

    ch1 with fuzz tutorial

---
+++ ch1/fuzz/reverse.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"unicode/utf8"
+)
+
+func Reverse(s string) (string, error) {
+
+	if !utf8.ValidString(s) {
+		return s, errors.New("input is not valid UTF-8")
+	}
+	b := []rune(s)
+
+	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return string(b), nil
+}
+
+func main() {
+	input := "The quick brown fox jumped over the lazy dog"
+	rev, revErr := Reverse(input)
+	doubleRev, doubleRevErr := Reverse(rev)
+
+	fmt.Printf("original %q\n", input)
+	fmt.Printf("reversed: %q, err: %v\n", rev, revErr)
+	fmt.Printf("reversed again: %q, err: %v\n", doubleRev, doubleRevErr)
+
+}
